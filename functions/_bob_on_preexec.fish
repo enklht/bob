0639fcca20a2c9d465ecce6367ca13ce9e1898b9
enklht <enklht@gmail.com>
2026-04-30 15:52:52 +0900

    fix: :bug: Use frozen history for search

--- functions/_bob_on_preexec.fish
+++ functions/_bob_on_preexec.fish
@@ -1,9 +1,10 @@
 function _bob_on_preexec --on-event fish_preexec -a command
-    set found_entries (builtin history search --case-sensitive --exact "$command")
-
-    # If a command is in the history and in the queue, ignore it, like if it wasn’t in the history
-    if test (count $found_entries) -gt 0; and not contains "$command" $_bob_queue
-        set --global _bob_found_in_history true
+    # If a command is in the history and not in the queue, ignore it, like if it wasn’t in the history
+    if contains $command $_bob_history
+        if not contains $command $_bob_queue
+            set --global _bob_found_in_history true
+        else
+        end
     else
         set --global _bob_found_in_history false
     end
--- functions/_bob_on_prompt.fish
+++ functions/_bob_on_prompt.fish
@@ -1,4 +1,5 @@
 function _bob_on_prompt --on-event fish_prompt
+    set --global _bob_history $history
     if test $bob_purge_only_on_exit = false
         _bob_remove_from_history
     end
